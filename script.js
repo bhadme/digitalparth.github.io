843374502606508a31b5df7cfecb3a71568773c6
bhadme <97607828+bhadme@users.noreply.github.com>
2022-01-12 19:57:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -41,12 +41,7 @@ $(document).ready(function(){
         loop: true
     });
 
-    var typed = new Typed(".typing-2", {
-        strings: ["Digital entreprenuer", "Digital entreprenuer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+  
 
     // owl carousel script
     $('.carousel').owlCarousel({
